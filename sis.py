8ff2c1374361f9a91277ef57b2bfb33d7317f67a
lichunjiang42@gmail.com <8589934593>
2016-11-21 00:44:51 +0800

    update
    update

--- sis.py
+++ sis.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-def frist_page():
-    url="http://174.127.195.202/bbs/forum-213-1.html"
+def login_page():
+    
     login_url="http://174.127.195.202/bbs/logging.php?action=login"
     r=requests.Session()
     parms={'formhash':'5c0103b6',
@@ -14,15 +14,18 @@ def frist_page():
     'answer':'',
     'loginsubmit':'true'}
     re=r.post(login_url,data=parms)
-    new=r.get(url,cookies=re.cookies)
-
+    frist_page(re.cookies)
+def frist_page(lcookies):
+    url="http://174.127.195.202/bbs/forum-213-1.html"
+    new=requests.get(url,cookies=lcookies)
+    
     print new.content
-    soup=BeautifulSoup(new.content,"html5lib")
-def get_info(url):
+    soup=BeautifulSoup(new.content,"html5lib")    
+def get_page_info(url,login_cookies):
     pass
 def db_check():
     pass
     
 if __name__=="__main__":
-    pass
+    login_page()
     
